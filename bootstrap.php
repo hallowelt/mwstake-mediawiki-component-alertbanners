de3a64ff71e264329ef6af4f23c9b4bb749b618e
Robert Vogel <1201528+osnard@users.noreply.github.com>
2022-02-07 16:10:07 +0100

    Use new `componentloader` (#2)
    * Test bootstrapper
    
    * Update bootstrap.php
    
    * Add new init requirement to `README.md`
    
    * Fix link
    
    * Fix version
    
    Co-authored-by: rvogel <vogel@hallowelt.biz>

--- bootstrap.php
+++ bootstrap.php
@@ -6,25 +6,28 @@ if ( defined( 'MWSTAKE_MEDIAWIKI_COMPONENT_ALERTBANNERS_VERSION' ) ) {
 
 define( 'MWSTAKE_MEDIAWIKI_COMPONENT_ALERTBANNERS_VERSION', '1.0.0' );
 
-$GLOBALS['mwsgAlertBannersProviderRegistry'] = [];
+MWStake\MediaWiki\ComponentLoader\Bootstrapper::getInstance()
+->register( 'alertbanners', function() {
+	$GLOBALS['mwsgAlertBannersProviderRegistry'] = [];
 
-$GLOBALS['mwsgAlertBannersProviderFactories'] = [
-	'globalvars-config' => [
-		'class' => "\\MWStake\\MediaWiki\\Component\\AlertBanners\\AlertProviderFactory\\GlobalVars",
-		'services' => [ 'ObjectFactory' ]
-	],
-	'hook' => [
-		'class' => "\\MWStake\\MediaWiki\\Component\\AlertBanners\\AlertProviderFactory\\Hook",
-		'services' => [ 'ObjectFactory', 'HookContainer' ]
-	]
-];
+	$GLOBALS['mwsgAlertBannersProviderFactories'] = [
+		'globalvars-config' => [
+			'class' => "\\MWStake\\MediaWiki\\Component\\AlertBanners\\AlertProviderFactory\\GlobalVars",
+			'services' => [ 'ObjectFactory' ]
+		],
+		'hook' => [
+			'class' => "\\MWStake\\MediaWiki\\Component\\AlertBanners\\AlertProviderFactory\\Hook",
+			'services' => [ 'ObjectFactory', 'HookContainer' ]
+		]
+	];
 
-$GLOBALS['wgHooks']['SiteNoticeAfter'][] =
-	'MWStake\\MediaWiki\\Component\\AlertBanners\\Hook\\SiteNoticeAfter\\AddAlerts::callback';
+	$GLOBALS['wgHooks']['SiteNoticeAfter'][] =
+		'MWStake\\MediaWiki\\Component\\AlertBanners\\Hook\\SiteNoticeAfter\\AddAlerts::callback';
 
-$GLOBALS['wgResourceModules']['mwstake.component.alertbanners'] = [
-	'scripts' => [
-		'resources/mwstake.components.alertbanners.js'
-	],
-	'localBasePath' => __DIR__
-];
+	$GLOBALS['wgResourceModules']['mwstake.component.alertbanners'] = [
+		'scripts' => [
+			'resources/mwstake.components.alertbanners.js'
+		],
+		'localBasePath' => __DIR__
+	];
+} );
