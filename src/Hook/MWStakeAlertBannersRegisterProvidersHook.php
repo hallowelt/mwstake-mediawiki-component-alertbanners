1884bbd76a5f1821f2edd10de3be9daa93dbb482
rvogel <vogel@hallowelt.biz>
2022-05-06 13:37:36 +0200

    Add hook interface

---
+++ src/Hook/MWStakeAlertBannersRegisterProvidersHook.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace MWStake\MediaWiki\Component\AlertBanners\Hook\SiteNoticeAfter;
+
+interface MWStakeAlertBannersRegisterProvidersHook {
+
+	/**
+	 * Allows to add `ObjectFactory` compatible specs. A unique key must be provided.
+	 *
+	 * @param array &$providerSpecs
+	 * @return void
+	 */
+	public function onMWStakeAlertBannersRegisterProviders( &$providerSpecs ): void;
+}
