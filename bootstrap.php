97668eb5aab247fd444828858cfdd322ebd2f620
Robert Vogel <1201528+osnard@users.noreply.github.com>
2022-05-27 11:15:07 +0200

    Bump version number

--- bootstrap.php
+++ bootstrap.php
@@ -4,7 +4,7 @@ if ( defined( 'MWSTAKE_MEDIAWIKI_COMPONENT_ALERTBANNERS_VERSION' ) ) {
 	return;
 }
 
-define( 'MWSTAKE_MEDIAWIKI_COMPONENT_ALERTBANNERS_VERSION', '2.0.3' );
+define( 'MWSTAKE_MEDIAWIKI_COMPONENT_ALERTBANNERS_VERSION', '2.0.4' );
 
 MWStake\MediaWiki\ComponentLoader\Bootstrapper::getInstance()
 ->register( 'alertbanners', static function () {
