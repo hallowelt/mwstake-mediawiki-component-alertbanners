00d9d3e483c0a5a69bd5c2c0ff4900030c85560d
Hamish <26261210+HamishSlater@users.noreply.github.com>
2022-10-19 11:56:15 +0200

    Fix 'Wikimedia\ObjectFactory' namespace

--- bootstrap.php
+++ bootstrap.php
@@ -4,7 +4,7 @@ if ( defined( 'MWSTAKE_MEDIAWIKI_COMPONENT_ALERTBANNERS_VERSION' ) ) {
 	return;
 }
 
-define( 'MWSTAKE_MEDIAWIKI_COMPONENT_ALERTBANNERS_VERSION', '2.0.4' );
+define( 'MWSTAKE_MEDIAWIKI_COMPONENT_ALERTBANNERS_VERSION', '2.0.5' );
 
 MWStake\MediaWiki\ComponentLoader\Bootstrapper::getInstance()
 ->register( 'alertbanners', static function () {
